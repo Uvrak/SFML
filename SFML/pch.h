84752e77453c1caeb0b5131811f5af8b6d9f9a68
Daniel Horn <danielhorn101@gmail.com>
2020-07-12 17:06:20 +0200

    Projektdateien hinzufügen.

---
+++ SFML/pch.h
@@ -0,0 +1,21 @@
+#include <limits>
+#include <vector>
+#include <string>
+#include <tchar.h>
+#include <stdio.h>
+#include<io.h>
+#include <map>
+#include <fstream>
+#include <sstream>
+
+#include <iostream>
+#include <conio.h>
+
+#include "SFML\System.hpp"
+#include "SFML\Window.hpp"
+#include "SFML\Graphics.hpp"
+#include "SFML\Audio.hpp"
+#include "SFML\Network.hpp"
+
+
+
